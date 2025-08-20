cb89748e3f310882ba6b73ff944ab6e65b34d1ed
CyberStackPro <yeabsragebriel@gmail.com>
2025-08-20 10:50:27 +0300

    build complete  directed and undirectd graph

---
+++ javascript/graphs/directed-graph.js
@@ -0,0 +1,115 @@
+class GraphNode {
+  constructor(value) {
+    this.value = value;
+    this.edges = [];
+  }
+}
+
+class DirectedGraph {
+  constructor() {
+    this.adjacencyList = new Map();
+  }
+
+  addNode(value) {
+    if (!this.adjacencyList.has(value)) {
+      const newNode = new GraphNode(value);
+      this.adjacencyList.set(value, newNode);
+    }
+    // console.log(this.adjacencyList);
+  }
+
+  addEdge(node1, node2) {
+    if (node1 === null || node2 === null) {
+      throw new Error("node can't be null");
+    }
+    if (!this.adjacencyList.has(node1) || !this.adjacencyList.has(node2)) {
+      throw new Error("Node not found");
+    }
+
+    let nodeA = this.adjacencyList.get(node1);
+    if (!nodeA.edges.includes(node2)) {
+      nodeA.edges.push(node2);
+    }
+    return;
+  }
+
+  removeEdge(node1, node2) {
+    if (!this.adjacencyList.has(node1) || !this.adjacencyList.has(node2)) {
+      throw new Error("Node not found");
+    }
+    // if (node1 === node2) {
+    //   throw new Error(
+    //     "Cannot remove  a node to itself. this is [Undirected] Graph"
+    //   );
+    // }
+
+    let nodeA = this.adjacencyList.get(node1);
+    if (nodeA.edges.includes(node2)) {
+      nodeA.edges = nodeA.edges.filter((n) => n !== node2);
+    }
+    return;
+  }
+
+  removeNode(value) {
+    if (!this.adjacencyList.get(value)) {
+      throw new Error(`${value} is not found on Graph`);
+    }
+    for (const [key, node] of this.adjacencyList) {
+      const index = node.edges.indexOf(value);
+      if (index !== -1) {
+        node.edges.splice(index, 1);
+      }
+    }
+    this.adjacencyList.delete(value);
+  }
+  hasEdge(node1, node2) {
+    if (!this.adjacencyList.has(node1)) return false;
+    return this.adjacencyList.get(node1).edges.includes(node2);
+  }
+  numberOfEdges() {
+    let count = 0;
+
+    for (const [key, node] of this.adjacencyList) {
+      count += node.edges.length;
+    }
+    return count;
+  }
+  getNeighbors(node) {
+    if (!this.adjacencyList.has(node)) {
+      throw new Error("Node not found");
+    }
+    let nodeA = this.adjacencyList.get(node);
+    return nodeA.edges;
+  }
+
+  size() {
+    return this.adjacencyList.size;
+  }
+
+  clear() {
+    this.adjacencyList.clear();
+  }
+
+  printPretty() {
+    for (const [key, node] of this.adjacencyList) {
+      let edges = node.edges.length ? node.edges.join(", ") : "∅";
+      console.log(`${key} -> ${edges}`);
+    }
+  }
+}
+
+const graph = new DirectedGraph();
+
+graph.addNode("A");
+graph.addNode("B");
+graph.addNode("C");
+graph.addNode("D");
+
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+graph.addEdge("B", "D");
+
+console.log(graph.numberOfEdges());
+
+graph.printPretty();
+// console.dir(graph, { depth: null });
--- javascript/graphs/undirected-graph.js
+++ javascript/graphs/undirected-graph.js
@@ -68,6 +68,8 @@ class UndirectedGraph {
       throw new Error(`${value} is not found on Graph`);
     }
     for (const [key, node] of this.adjacencyList) {
+      // console.log("KEY: ", node.edges.indexOf(value));
+
       const index = node.edges.indexOf(value);
       if (index !== -1) {
         node.edges.splice(index, 1);
@@ -76,11 +78,70 @@ class UndirectedGraph {
 
     this.adjacencyList.delete(value);
   }
-  removeEdge() {}
+  removeEdge(node1, node2) {
+    if (!this.adjacencyList.has(node1) || !this.adjacencyList.has(node2)) {
+      throw new Error("Node not found");
+    }
+    if (node1 === node2) {
+      throw new Error(
+        "Cannot remove  a node to itself. this is [Undirected] Graph"
+      );
+    }
+
+    let nodeA = this.adjacencyList.get(node1);
+    if (nodeA.edges.includes(node2)) {
+      nodeA.edges = nodeA.edges.filter((n) => n !== node2);
+    }
+
+    let nodeB = this.adjacencyList.get(node2);
+    if (nodeB.edges.includes(node1)) {
+      nodeB.edges = nodeB.edges.filter((n) => n !== node1);
+    }
+
+    return;
+  }
+
+  hasEdge(node1, node2) {
+    // let nodeB = this.adjacencyList.get(node2);
+    // if (nodeB.edges.includes(node1)) {
+    //   return true;
+    // }
+
+    // return false;
+
+    return this.adjacencyList.get(node1).edges.includes(node2);
+  }
+
+  getNeighbors(node) {
+    if (!this.adjacencyList.has(node)) {
+      throw new Error("Node not found");
+    }
+    let nodeA = this.adjacencyList.get(node);
+    return nodeA.edges;
+  }
+  numberOfEdges() {
+    let count = 0;
+
+    for (let [key, node] of this.adjacencyList) {
+      // console.log(node);
+      count += node.edges.length;
+    }
+
+    return count / 2;
+  }
+
+  size() {
+    return this.adjacencyList.size;
+  }
+
+  clear() {
+    this.adjacencyList.clear();
+  }
 
   printPretty() {
     for (const [key, node] of this.adjacencyList) {
-      console.log(`${key}: ${JSON.stringify(node.edges)}`);
+      // console.log(`${key}: ${JSON.stringify(node.edges)}`);
+      console.log(`${key}: ${node.edges.join(", ")}`);
     }
   }
 }
@@ -89,15 +150,27 @@ const graph = new UndirectedGraph();
 
 graph.addNode("A");
 graph.addNode("B");
-graph.addNode("C");
+// graph.addNode("C");
 
-graph.addEdge("A", "C");
-graph.addNode("D");
-graph.addEdge("B", "D");
-graph.addEdge("C", "D");
+// graph.addEdge("A", "C");
+// graph.addEdge("A", "B");
+
+// graph.addNode("D");
+// graph.addEdge("B", "D");
+// graph.addEdge("C", "D");
+
+// graph.removeEdge("A", "C");
+
+// console.log(graph.hasEdge("A", "C"));
+
+// console.log(graph.getNeighbors("C"));
+
+// graph.clear();
+
+console.log(graph.numberOfEdges());
 
 // graph.removeNode("C");
 
 graph.printPretty();
 
-console.dir(graph, { depth: null });
+// console.dir(graph, { depth: null });
