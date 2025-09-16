f719d9ba79c1212b0a303f471e2dcabfd1a2aab3
CyberStackPro <yeabsragebriel@gmail.com>
2025-09-16 11:53:28 +0300

    implement prirority queue and refine the old one

--- javascript/queues/prirority-queue.js
+++ javascript/queues/prirority-queue.js
@@ -1,26 +1,51 @@
 class PrirorityQueue {
-  constructor(element, prirority, capacity) {
+  constructor(comparator = (a, b) => a.priority - b.priority) {
     // this.element = element;
     // this.prirority = prirority;
     this.items = [];
-    this.count = 0;
+    // this.count = 0;
+    this.comparator = comparator;
   }
 
-  add(item) {
-    // Shifting Items
-    let i;
-    for (i = this.count - 1; i >= 0; i--) {
-      if (this.items[i] > item) this.items[i + 1] = this.items[i];
-      else break;
+  // add(item) {
+  //   // Shifting Items
+  //   let i;
+  //   for (i = this.count - 1; i >= 0; i--) {
+  //     if (this.items[i] > item) this.items[i + 1] = this.items[i];
+  //     else break;
+  //   }
+  //   this.items[i + 1] = item;
+  //   this.count++;
+  // }
+
+  enqueue(element, priority) {
+    const queueElement = { element, priority };
+    let i = 0;
+    while (
+      i < this.items.length &&
+      this.comparator(this.items[i], queueElement) <= 0
+    ) {
+      i++;
     }
-    this.items[i + 1] = item;
-    this.count++;
+    this.items.splice(i, 0, queueElement);
+  }
+
+  dequeue() {
+    return this.items.shift();
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  peek() {
+    return this.items[0];
   }
 }
 
 const queue = new PrirorityQueue();
-queue.add(10);
-queue.add(50);
-queue.add(20);
-queue.add(30);
+queue.enqueue(10, 1);
+queue.enqueue(50, 4);
+queue.enqueue(20, 3);
+queue.enqueue(30, 2);
 console.log(queue);
